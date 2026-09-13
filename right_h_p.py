5063abe7640979bf0bbcf25def20f9f53a748a2e
John Paul <alappatjohnpaul@gmail.com>
2026-09-13 11:24:16 +0530

    Number_p

--- right_h_p.py
+++ right_h_p.py
@@ -10,5 +10,11 @@ def patterns(n):
         for _ in range(i):
             print("*",end=" ")
         print()
-patterns(5)
+# patterns(5)
 
+def pattern3(n):
+    for i in range(n+1,0,-1):
+        for j in range(1,i):
+            print(j,end=" ")
+        print()
+pattern3(5)
